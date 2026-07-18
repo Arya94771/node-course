6b6b8acdcd93a5da3792ed709e003ec5fadc1fe9
sri_arya16 <aryasrivastav9270@gmail.com>
2026-07-19 01:20:38 +0530

    Different syntaxes of modules

---
+++ 03-modules.js
@@ -0,0 +1,11 @@
+const names=require('./names');
+console.log(names);
+
+const sayHi=require('./utils');
+const data=require('./04-alternatives for modules')
+console.log(data);
+sayHi('tony');
+sayHi(names.name1);
+sayHi(names.name2);
+
+
---
+++ names.js
@@ -0,0 +1,5 @@
+const secret='Super secret';
+const name1='John';
+const name2='Peter Parker';
+
+module.exports={name1,name2};
